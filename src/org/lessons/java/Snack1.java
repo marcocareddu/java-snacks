52d7312d3d3070c6cf56fa71299d06768da79b6b
marcocareddu <saldumaccu@tuta.io>
2023-11-10 11:29:39 +0100

    first commit

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,25 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+public class Snack1 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+//		Inputs from user
+		System.out.println("Inserisci un numero");
+		String input = in.nextLine();
+		int number = Integer.valueOf(input);
+		
+//		Calc even/odd and print
+		if((number %2) == 0) {
+			System.out.println("Il numero " + number + " è pari!");	
+		} else {
+			System.out.println("Il numero successivo è " + (number + 1));
+		}
+		
+	}
+}
