c68df4f39f2fb095a4b47dc41ff16e94ea1c43db
marcocareddu <saldumaccu@tuta.io>
2023-11-10 17:10:51 +0100

    Added Snack 10

---
+++ src/org/lessons/java/Snack10.java
@@ -0,0 +1,36 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+/*
+ * Dati due numeri interi acquisiti dall'utente (Scanner), 
+ * calcolare la somma di tutti i numeri 
+ * compresi tra questi due valori (inclusi).
+ */
+
+public class Snack10 {
+
+	public static void main(String[] args) {
+		
+//		User inputs
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserisci il primo numero: ");
+		int number1 = in.nextInt();
+		
+		System.out.println("Inserisci il secondo numero: ");
+		int number2 = in.nextInt();
+		
+//		Vars
+		int difference = number2 - number1;
+		int number = 0;
+		
+//		Calc
+		for(int i = 0; i <= difference; i++) {
+			number += number1 + i;
+		}
+		
+//		Print result
+		System.out.println(number);
+	}
+}
