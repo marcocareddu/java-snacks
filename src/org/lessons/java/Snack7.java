d34ed3f0c01105cc4d3d3b842d28504308e7430d
marcocareddu <saldumaccu@tuta.io>
2023-11-10 15:40:55 +0100

    Added Snack 7

--- src/org/lessons/java/Snack7.java
+++ src/org/lessons/java/Snack7.java
@@ -1,5 +1,31 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
+/*Scrivere un programma che dati dei secondi li converta in 
+ * ore, minuti, secondi e mostri a video 
+ * la stringa generata (secondi → “hh:mm:ss”)
+ */
 public class Snack7 {
 
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+//		User input
+		System.out.println("Inserisci i secondi; ");
+		int input = in.nextInt();
+		
+//		Close input
+		in.close();
+		
+//		Calc
+		int hours = input / 3600;
+        int minutes = (input % 3600) / 60;
+        int seconds = input % 60;
+
+        String humanClock = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+        System.out.println(input + " secondi corrispondono a : " + humanClock);
+	}
 }
